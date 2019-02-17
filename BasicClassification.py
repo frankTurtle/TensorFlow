95db8306be18b61ce776ef53c853ac788ec4e60e
Barret J. Nobel - <bear.nobel@protonmail.com>
2019-02-17 09:31:12 -0500

    added a few steps and comments

--- BasicClassification.py
+++ BasicClassification.py
@@ -9,3 +9,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# preprocessing both training and testing data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+
+## BUILDING THE MODEL ##
+
+#1 Setup the layers
+# flatten converts from 2d array of 28*28 to 1d of 784
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# compile the model
+# loss - measures how accurate the model is during training
+# optimizer - how the model is updated
+# metrics - used to monitor the training and testing steps
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
